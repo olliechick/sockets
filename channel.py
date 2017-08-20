c73b3f26656f657886c6bc30aed7d89e77082d41
Ollie Chick <och26@uclive.ac.nz>
2017-08-21 02:02:17 +1200

    Modified channel.py: made process_packet() not modify data for testing, add various print statements for testing, made command line argument errors more graceful, add checks for port number validity

--- channel.py
+++ channel.py
@@ -18,9 +18,13 @@ DROP_RATE = 0
 
 
 def process_packet(data):
-    """Process an input packet and randomly drop it or change its header"""
+    """Process an input packet (as bytes) and randomly drop it or change its header.
+    Returns the input packet as bytes.
+    """
+    if True: #so Wing doesn't complain
+        return data #just for testing
     p = packet.Packet()
-    p.byte_deconversion(data)
+    p.decode(data)
     print(p.data)
     if p.magic_no != 0x497E: #drop if magic number different
         return None
@@ -29,30 +33,36 @@ def process_packet(data):
     elif random.uniform(0,1) < BIT_ERR_RATE: #create a bit error
         p.data_len += int(random.uniform(1, 10))
     
-    return p.byte_conversion() #return the packets byte conversion
+    return p.encode() #return the packet's byte conversion
 
 
 def main_loop(sender_in, sender_out, recv_in, recv_out):
     """
-       Wait to recieve packets on sender_in and recv_in. When it does
+       Wait to recieve packets on sender_in and recv_in. When it does,
        process the packet and send it on to either recv_out or sender_out
-       respectively. Takes the four socket objects as arguments
+       respectively. Takes the four socket objects as arguments.
     """
     while True:
+        print("Waiting...", flush = True)
+        
         readable, _, _ = select.select([sender_in, recv_in], [], [])
         
         for s in readable:
             conn, addr = s.accept()
             data = conn.recv(1024)
+            print("Got some data:", data)
             
             data_to_forward = process_packet(data)
             ##NOT SURE IF THIS PART WORKS
             if data_to_forward != None: #if the packet isn't dropped
-                print("Sending data back")
-                if s.getsockname() == sender_in.getsockname(): #came from sender send to receiver
+                if s.getsockname() == sender_in.getsockname(): #came from sender, send to receiver
+                    print("Forwarding data to receiver.")
                     recv_out.sendall(data_to_forward)
+                    print("Sent.")
                 else: #else send to sender
+                    print("Forwarding data to sender.")
                     sender_out.sendall(data_to_forward)
+                    print("Sent.")
 
 
 def main(args):
@@ -74,12 +84,22 @@ def main(args):
         
         #Probability of dropping a packet
         DROP_RATE = float(args[7])
-    except ValueError:
-        sys.exit("""Usage: {} <sender_in_port> <sender_out_port> <recv_in_port> <recv_out_port> <sender> <recv> <drop_rate>""".format)
+    except:
+        print("""Usage: {} <sender_in_port> <sender_out_port> <recv_in_port> <recv_out_port> <sender> <recv> <drop_rate>""".format(args[0]))
+        return
+    
+    # Check that ports are in the valid range
+    for port in [sender_in_port, sender_out_port, recv_in_port, \
+                 recv_out_port, sender, recv]:
+        if port < 1024 or port > 64000:
+            print("All port numbers should be in the range [1024, 64000].")
+            return
+        
         
     #Check that the drop rate is between 0 and 1 (inclusive)
     if (DROP_RATE > 1) or (DROP_RATE < 0):
-        sys.exit("drop_rate must be an float between 0 and 1") 
+        print("drop_rate should be in the range [0, 1).") 
+        return
     
     try:
         #Create the socket to be connected to by sender's out port
@@ -114,15 +134,15 @@ def main(args):
         #Create the socket to connect to recievers in port
         recv_out = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         recv_out.bind(("", recv_out_port))
-        #recv_out.connect(("", recv))
+        recv_out.connect(("", recv))
         print("Started recv_out")
     except IOError:
         sys.exit("An IO Error occurred trying to connect to receiver at port {}".format(recv))
 
-    main_loop(sender_in, 12, recv_in, recv_out)
+    main_loop(sender_in, sender_out, recv_in, recv_out)
 
 
 if __name__ == "__main__":
-    ##args = sys.argv[]
+    args = sys.argv
     args = ['channel.py', 15620, 15621, 15622, 15623, 15630, 15640, 1]
     main(args)
