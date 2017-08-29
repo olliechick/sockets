a18143fb9128fcc77c1a5655d77822caa1855c41
Ollie Chick <och26@uclive.ac.nz>
2017-08-29 15:09:09 +1200

    Rename cleaning_sender to sender

--- sender.py
+++ sender.py
@@ -3,16 +3,52 @@ A program to send packets to a channel.
 For a COSC264 assignment.
 
 Author: Ollie Chick and Samuel Pell
-Date modified: 26 August 2017
+Date modified: 29 August 2017
 """
 
 import sys, socket, os, packet, select
 
 MAGIC_NO = 0x497E
 IP = '127.0.0.1'
+TIMEOUT = 3 #seconds
+FILE_ENCODING = 'utf8'
+
+def inner_loop(socket_out, socket_in, bytes_to_send, next_no):
+    """
+       Function to continuously send a packet until a valid acknowledgement
+       packet is recieved. Returns the number of packets sent from sender to
+       achieve successful transmission.
+    """
+    print(bytes_to_send)
+    packets_sent = 0
+
+    while True:
+        # Send packet
+        socket_out.send(bytes_to_send)
+        print("Packet sent...")
+        packets_sent += 1
+
+        # Await a response
+        readable, _, _ = select.select([socket_in], [], [], TIMEOUT)
+
+        if readable:
+            #got a response
+            print('Response received.')
+            s = readable[0]
+            data = s.recv(1024)
+
+            rcvd = packet.Packet()
+            rcvd.decode(data)
+
+            if rcvd.is_valid_ack(next_no):
+                #got a valid acknowledgement packet
+                next_no = 1 - next_no
+                return packets_sent, next_no
+        else:
+            print("No response, retransmitting.")
+
 
 def main(args):
-
     # Check arguments are valid
     try:
         in_port = int(args[1])
@@ -30,7 +66,7 @@ def main(args):
 
     packets_sent = 0
 
-    # Create sockets
+    # Create sockets and connect socket_out
     try:
         socket_in = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_in.bind((IP, in_port))
@@ -44,19 +80,13 @@ def main(args):
         socket_out = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_out.bind((IP, out_port))
         print("Started socket_out at port", out_port)
-    except IOError: #If it fails give up and go home
-        socket_in.close()
-        socket_out.close()
-        sys.exit("An IO Error occurred trying to create socket_out.")
-
-    # Connect out port to channel sender in port
-    try:
         socket_out.connect((IP, channel_in_port))
         print("Connected socket_out to port", channel_in_port)
     except IOError: #If it fails give up and go home
         socket_in.close()
         socket_out.close()
-        sys.exit("An IO Error occurred trying to connect socket_out. Port:{}".format(channel_in_port))
+        sys.exit("An IO Error occurred trying to create and connect socket_out.")
+
 
     # Check if file exists
     if not os.path.isfile(filename):
@@ -69,73 +99,45 @@ def main(args):
     file = open(filename, "rb")
     input("Please acknowledge on the channel that you have started the sender, then press enter.")
 
-
+    # Accept connection from channel
     socket_in, addr = socket_in.accept()
 
     # Outer loop
-    while True:
+    while not exit_flag:
         print("\n\n\n")
-        # Read 512 bytes from filename
+        # Read 512 bytes from file
         data = file.read(512)
-        #print("Read data:", data)
-        print("Successfully read data")
-        data = data.decode('utf8')
+        ##print("Read data:", data)
+        print("Successfully read data.")
+        data = data.decode(FILE_ENCODING)
 
         # Prepare packet
-
         packet_type = packet.PTYPE_DATA
         seq_no = next_no
         data_len = len(data)
         if data_len == 0:
             exit_flag = True
+
         pack = packet.Packet(MAGIC_NO, packet_type, seq_no, data_len, data)
         print('Packet of length', len(pack))
 
         # Inner loop
-        return_to_outer_loop = False
         bytes_to_send = pack.encode()
 
-        while True and not return_to_outer_loop:
-            # Send packet
-            socket_out.send(bytes_to_send)
-            packets_sent += 1
+        packets_used, next_no = inner_loop(socket_out, socket_in, bytes_to_send,
+                                           next_no)
 
-            # Await a response
-            timeout = 3 #seconds
-            readable, _, _ = select.select([socket_in], [], [], timeout)
-
-            if readable:
-                #got a response
-                print('got a response')
-                s = readable[0]
-                data = s.recv(1024)
-
-                rcvd = packet.Packet()
-                rcvd.decode(data)
-
-                if rcvd.magic_no == MAGIC_NO and rcvd.packet_type == packet.PTYPE_ACK \
-                   and rcvd.data_len == 0 and rcvd.seq_no == next_no:
-                    next_no = 1 - next_no
-
-                    if exit_flag:
-                        print("WARNING! CLOSING SOCKETS!")
-                        file.close()
-                        socket_in.shutdown(socket.SHUT_RDWR)
-                        socket_in.close()
-                        socket_out.shutdown(socket.SHUT_RDWR)
-                        socket_out.close()
-                        print(packets_sent, "packets sent.")
-                        return
-                    else:
-                        return_to_outer_loop = True
-            else:
-                print("No response.")
+        packets_sent += packets_used
 
 
+
+
+    #clean up and close
     print("WARNING! CLOSING SOCKETS!")
     file.close()
     socket_in.close()
     socket_out.close()
+    print("Packets sent: {}".format(packets_sent))
 
 
 if __name__ == "__main__":
@@ -146,15 +148,9 @@ if __name__ == "__main__":
     # * a file name, indicating the file to send
     args = sys.argv
 
-    # Available files:
-    # Name          Size (characters)
-    #
-    # data.txt      1473
-    # small.txt     6
-
     filename = 'input'
 
-    s_in, s_out, c_s_in, c_s_out, c_r_in, c_r_out, r_in, r_out = packet.get_socket_numbers()
-    args = ['sender.py', s_in, s_out, c_s_in, filename] ##this is just for testing
+    s_in, s_out, c_s_in, c_s_out, c_r_in, c_r_out, r_in, r_out = packet.get_socket_numbers() ##just for testing
+    args = ['sender.py', s_in, s_out, c_s_in, filename] ##just for testing
 
     main(args)
