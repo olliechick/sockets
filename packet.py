9d1144ca749e00b865435626f9b8d807dec446fb
Ollie Chick <och26@uclive.ac.nz>
2017-09-01 22:07:25 +1200

    Add checksum to packet (and checks)

--- packet.py
+++ packet.py
@@ -27,6 +27,7 @@ class Packet:
         self.packet_type = packet_type #either dataPacket or acknowledgementPacket
         self.seq_no = seq_no           #sequence number
         self.data_len = data_len       #number of bytes in the data
+        self.checksum = (magic_no + packet_type + seq_no + data_len) % 1000
         self.data = data               #data carried by packet
 
 
@@ -59,8 +60,23 @@ class Packet:
         conv += str(self.packet_type)
         conv += str(self.seq_no)
         conv += "0" * (3 - len(str(self.data_len))) + str(self.data_len)
+        conv += "0" * (3 - len(str(self.checksum))) + str(self.checksum)
         conv += str(self.data)
         return bytes(conv, "utf-8")
+
+
+    def decode(self, data):
+        """Sets the fields of this packet to that of data"""
+        try:
+            data = data.decode()
+            self.magic_no = int(data[:5])
+            self.packet_type = int(data[5])
+            self.seq_no = int(data[6])
+            self.data_len = int(data[7:10])
+            self.checksum = int(data[10:13])
+            self.data = data[13:]
+        except:
+            print("Error decoding data ({}). Packet is unchanged.".format(data))
 
 
     def is_valid_ack(self, next_no):
@@ -72,18 +88,14 @@ class Packet:
         valid_type = self.packet_type == PTYPE_ACK
         valid_length = self.data_len == 0
         valid_seq_no = self.seq_no == next_no
+        valid_checksum = self.checksum == (self.magic_no + self.packet_type + \
+                                           self.seq_no + self.data_len) % 1000
+        return valid_magic and valid_type and valid_length and valid_seq_no and valid_checksum
 
-        return valid_magic and valid_type and valid_length and valid_seq_no
 
-
-    def decode(self, data):
-        """Sets the fields of this packet to that of data"""
-        try:
-            data = data.decode()
-            self.magic_no = int(data[:5])
-            self.packet_type = int(data[5])
-            self.seq_no = int(data[6])
-            self.data_len = int(data[7:10])
-            self.data = data[10:]
-        except:
-            print("Error decoding data. Packet is unchanged.")
+    def is_valid_ack(self):
+        valid_magic = self.magic_no == MAGIC_NO
+        valid_type = self.packet_type == PTYPE_DATA
+        valid_checksum = self.checksum == (self.magic_no + self.packet_type + \
+                                           self.seq_no + self.data_len) % 1000
+        return valid_magic and valid_type and valid_checksum
